1d3f385144d703028f8afda868ceea929cc548e4
Dtyj <80689943+Dtyj03@users.noreply.github.com>
2023-07-03 14:01:11 +0800

    Delete client.js

--- client.js
+++
@@ -1,6 +0,0 @@
-import * as contentful from 'contentful';
-
-export const client = contentful.createClient({
-    space: 'zzfw6ketwj9x' ,
-    accessToken: 'DkhgfxwSxGvcGwQJNt9wIxTXSk-WVMcc4GNDiO4HLvM'
-})
